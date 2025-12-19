3125714b2495637b4c26da90bf61293cf4bff687
jethvak28-cyber <2304030100557@silveroakuni.ac.in>
2025-12-19 16:16:13 +0530

    commit

---
+++ src/Untitled-1.js
@@ -0,0 +1,11 @@
+// Simple greeting function
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+console.log(greet("World"));
+
+// Array operations
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map(n => n * 2);
+console.log(doubled);
